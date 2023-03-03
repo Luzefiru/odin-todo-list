51b57e8ade32ff9a23c74c579ecf9e2582bf407d
Luzefiru <ian.deejay.iv@gmail.com>
2023-03-03 14:01:43 +0800

    docs: remove unnecessary documentation

--- src/index.js
+++ src/index.js
@@ -2,11 +2,6 @@ import { Todo, createProject } from './model';
 import ViewModule from './view';
 import ProjectDataHandler from './controller';
 
-// const addTaskBtn = document.querySelector('.add-task-btn');
-// addTaskBtn.addEventListener('click', () => {
-
-// })
-
 // initiatlizes the default project & selects it for appending tasks with the Add A Task Form
 const home = createProject('Home');
 ProjectDataHandler.addProject(home);
