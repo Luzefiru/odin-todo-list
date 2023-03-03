190caec2f0550c82a8377ba44dda0f57e1fbd648
Luzefiru <ian.deejay.iv@gmail.com>
2023-03-03 10:39:02 +0800

    feat: revert changes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import { Todo, createProject } from './model';
-import SidebarFunctionalities from './controller';
 
 const eatFood = Todo.createTodoItem('Eat Food', 'eat food in the diner', 5, 'please do it');
 const sleep = Todo.createTodoItem('Sleep', 'sleepy time', 1, 'weeee');
