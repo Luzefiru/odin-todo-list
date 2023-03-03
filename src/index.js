b592d562b1a1be2936bf84b44d5078eac2e1efb2
Luzefiru <ian.deejay.iv@gmail.com>
2023-03-03 12:04:11 +0800

    feat: make javascript load form

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import { Todo, createProject } from './model';
+import ViewModule from './view';
 
 // const addTaskBtn = document.querySelector('.add-task-btn');
 // addTaskBtn.addEventListener('click', () => {
 
 // })
+
+ViewModule.renderAddTaskPrompt();
 
 const eatFood = Todo.createTodoItem('Eat Food', 'eat food in the diner', 5, 'please do it');
 const sleep = Todo.createTodoItem('Sleep', 'sleepy time', 1, 'weeee');
--- src/view.js
+++ src/view.js
@@ -1,5 +1,22 @@
-// const viewModule = function {
-//   const renderProject = function () {
+const ViewModule = (function () {
+  const renderAddTaskPrompt = function () {
+    const formInnerHTML = `<label class="form--add-task__label" for="title">Title</label>
+      <input class="form--add-task__input" id="title" type="text" name="Task Title" placeholder="What to do?" required>
+      <label class="form--add-task__label" for="desciption">Description (optional)</label>
+      <textarea class="form--add-task__input" id="description" type="text" name="Task Description" placeholder="e.g. Coordinate with teammates via Zoom"></textarea>
+      <label class="form--add-task__label" for="dueDate">Due Date</label>
+      <input class="form--add-task__input" id="dueDate" type="date" name="Task Due Date" required>
+      <div class="form--add-task__buttons">
+        <button class="buttons__add">Add</button><button class="buttons__cancel">Cancel</button>
+      </div>`;
+    // adds the form to the content container as the first child
+    const newForm = document.createElement('form');
+    newForm.classList.add('form--add-task');
+    newForm.innerHTML = formInnerHTML;
+    document.querySelector('.add-task-btn').before(newForm);
+  };
 
-//   }
-// }
+  return { renderAddTaskPrompt };
+})();
+
+export default ViewModule;
